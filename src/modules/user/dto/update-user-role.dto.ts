541ea52aeeb7f7ef4ca47235e6592125f2655325
ProgrammingWithHT <hamzach3163@gmail.com>
2026-06-07 20:47:35 +0500

    creating 3 diff microservese

--- src/modules/user/dto/update-user-role.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsEnum } from 'class-validator';
-import { UserRole } from '../../../common/enums/user-role.enum';
-
-export class UpdateUserRoleDto {
-  @IsEnum(UserRole)
-  role: UserRole;
-}
